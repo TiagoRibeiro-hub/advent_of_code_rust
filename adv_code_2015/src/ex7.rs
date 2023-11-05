34ffebb3b68ce7bbfc81389df12d42dec53a3639
tiago <tiagovrib@gmail.com>
2023-11-05 16:19:58 +0000

    aoc 2015 ex7 example

--- adv_code_2015/src/ex7.rs
+++ adv_code_2015/src/ex7.rs
@@ -15,7 +15,34 @@ pub fn get_wire_a() {
 
 #[test]
 fn test() {
+    example()
     // let res = lights_on();
     // assert_eq!(res, 569999);
     //println!("res- {:?}", res);
 }
+
+fn example() {
+    let x = 123;
+    let y = 456;
+    let d = x & y;
+    let e = x | y;
+    let f = x << 2;
+    let g = y >> 2;
+    let mut h = !x;
+    let mut i = !y;
+    
+    if h < 0 {
+        h = 65535 - x;
+    }
+    if i < 0 {
+        i = 65535 - y;
+    }
+    println!("{d}");// d: 72
+    println!("{e}");// e: 507
+    println!("{f}");// f: 492
+    println!("{g}");// g: 114
+    println!("{h}");// h: 65412
+    println!("{i}");// i: 65079
+    println!("{x}");// x: 123
+    println!("{y}");// y: 456
+    }
